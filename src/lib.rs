bcca2dac5ebffab938046976dd6ed4ab9d787cbc
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-02-04 09:07:11 +0000

    some optimisations

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(slice_swap_unchecked)]
+
 use num_complex::Complex;
 
 fn combine(zs: &[Complex<f64>], input: &mut [Complex<f64>]) {
@@ -5,12 +7,14 @@ fn combine(zs: &[Complex<f64>], input: &mut [Complex<f64>]) {
     let z_step = zs.len() / n;
 
     for k in 0..n / 2 {
-        let z = zs[k * z_step];
+        unsafe {
+            let z = *zs.get_unchecked(k * z_step);
 
-        let e = input[k];
-        let o = input[k + n / 2];
-        input[k] = e + z * o;
-        input[k + n / 2] = e - z * o;
+            let e = *input.get_unchecked(k);
+            let o = *input.get_unchecked(k + n / 2);
+            *input.get_unchecked_mut(k) = e + z * o;
+            *input.get_unchecked_mut(k + n / 2) = e - z * o;
+        }
     }
 }
 
@@ -21,15 +25,14 @@ pub fn fft_inplace(input: &mut [Complex<f64>]) {
     for i in 0..n {
         let j = i.reverse_bits() >> shift;
         if i < j {
-            input.swap(i, j);
+            unsafe { input.swap_unchecked(i, j) };
         }
     }
 
     let step = -2.0 * std::f64::consts::PI / (n as f64);
 
-    let zs: Vec<_> = (0..n / 2)
-        .map(|k| Complex::from_polar(1.0, (k as f64) * step))
-        .collect();
+    let mut zs = Vec::with_capacity(n / 2);
+    zs.extend((0..n / 2).map(|k| Complex::from_polar(1.0, (k as f64) * step)));
 
     let mut m = 2;
     while m <= n {
